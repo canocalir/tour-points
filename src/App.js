f8111ad486e9d8dbffe1c5b4b8a9ccdfc97eac76
Can Berk Ocalir <can.ocalir@gmail.com>
2022-10-08 13:22:38 +0300

    added Loading Component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
 import Header from './components/Header/Header';
+import Loading from './components/Loading/Loading';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   const [ tourplaces, setTourPlaces ] = useState([])
+  const [ loading, setLoading ] = useState(false)
 
   useEffect(() => {
     const fetchPlaces = async () => {
+      setLoading(true)
       const SERVER_URL = 'https://tourplaces-json-server.herokuapp.com/tourplaces'
       const res = await fetch(SERVER_URL)
       const data = await res.json()
       setTourPlaces(data)
+      setTimeout(()=>{
+        setLoading(false)
+      },1000)
     }
     fetchPlaces()
-  },[])
+  },[setTourPlaces])
 
   return (
     <div className="App">
       <Navbar/>
       <Header/>
-      <Card tourplaces={tourplaces}/>
+      {loading ? <Loading/> : <Card tourplaces={tourplaces}/>}
     </div>
   );
 }
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import './Loading.css'
+
+const Loading = () => {
+  return (
+    <div className="loading"></div>
+  )
+}
+
+export default Loading
