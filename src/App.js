2862f956a3bcf25de96ad41ca68fdcc4259cf44a
Can Berk Ocalir <can.ocalir@gmail.com>
2022-10-08 17:50:58 +0300

    fixed scss files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import './App.css';
+import './App.scss';
 import Card from './components/Card/Card';
 import Header from './components/Header/Header';
 import Loading from './components/Loading/Loading';
